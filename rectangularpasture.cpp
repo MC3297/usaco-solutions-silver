0f3039042df2c86338984a69b315a08066b9a801
MC3297 <97566000+MC3297@users.noreply.github.com>
2023-12-29 23:50:21 -0500

    Add files via upload

---
+++ rectangularpasture.cpp
@@ -0,0 +1,71 @@
+#pragma region
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+//#define int ll
+using pii = pair<int,int>;
+#ifndef LOCAL
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#define debug(...)
+#endif
+#define fi first
+#define se second
+#define pb push_back 
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)(x).size()
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define YES cout << "YES" << '\n'
+#define NO cout << "NO" << '\n'
+#define IMP cout << "IMPOSSIBLE" << '\n'
+#define FOR(i,s,e) for (int i = (s); i < (e); ++i) 
+#define F0R(i,e) FOR(i,0,e) 
+#define ROF(i,s,e) for (int i = (e)-1; i >= (s); --i) 
+#define R0F(i,e) ROF(i,0,e) 
+#define printv(v,s,e) FOR(i,s,e+1) cout << v[i] << ' '; cout << '\n'
+#define re(...) int __VA_ARGS__; read(__VA_ARGS__);
+template<class T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+template<class T, class H> istream& operator>>(istream& in, pair<H, T>& a) {in >> a.fi >> a.se; return in;};
+template<typename T> istream& operator>>(istream& in, vector<T>& a) {for(auto &x: a) in >> x; return in;};
+template<class T> void read(T &x) {cin >> x;}
+template<class H, class... T> void read(H &h, T &...t) { read(h); read(t...); }
+#pragma endregion
+
+
+#include <ext/pb_ds/assoc_container.hpp>//careful define int ll
+using namespace __gnu_pbds;
+template<typename T> using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+void solve() {
+    re(n);
+    vector<pii> stor(n);
+    read(stor);
+    sort(all(stor));
+    ll ans = 0;
+    F0R(i,n) {
+        ordered_set<int> s;
+        s.insert(stor[i].se);
+        FOR(j,i+1,n) {
+            s.insert(stor[j].se);
+            int mn = min(stor[i].se, stor[j].se);
+            int mx = max(stor[i].se, stor[j].se);
+            ans += (s.order_of_key(mn)+1)*(sz(s)-s.order_of_key(mx));
+        }
+    }
+    cout << ans+n+1 << '\n';
+}
+signed main() {
+    #ifdef LOCAL
+    freopen("txt.in","r",stdin);
+    freopen("txt.out","w",stdout);
+    #else
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    #endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
