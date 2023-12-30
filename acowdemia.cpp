2849ece63b2cb8c331ba66c4d9108a44d0308e6c
MC3297 <97566000+MC3297@users.noreply.github.com>
2023-12-30 03:40:10 -0500

    Add files via upload

---
+++ acowdemia.cpp
@@ -0,0 +1,73 @@
+#pragma region
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define int ll
+using pii = pair<int,int>;
+#ifndef LOCAL
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#define debug(...)
+#endif
+#define fi first
+#define se second
+#define pb push_back 
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)(x).size()
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define YES cout << "YES" << '\n'
+#define NO cout << "NO" << '\n'
+#define IMP cout << "IMPOSSIBLE" << '\n'
+#define FOR(i,s,e) for (int i = (s); i < (e); ++i) 
+#define F0R(i,e) FOR(i,0,e) 
+#define ROF(i,s,e) for (int i = (e)-1; i >= (s); --i) 
+#define R0F(i,e) ROF(i,0,e) 
+#define printv(v,s,e) FOR(i,s,e+1) cout << v[i] << ' '; cout << '\n'
+#define re(...) int __VA_ARGS__; read(__VA_ARGS__);
+template<class T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+template<class T, class H> istream& operator>>(istream& in, pair<H, T>& a) {in >> a.fi >> a.se; return in;};
+template<typename T> istream& operator>>(istream& in, vector<T>& a) {for(auto &x: a) in >> x; return in;};
+template<class T> void read(T &x) {cin >> x;}
+template<class H, class... T> void read(H &h, T &...t) { read(h); read(t...); }
+#pragma endregion
+
+int n,k,l;
+vector<int> stor, pref;
+bool good(int h) {
+    if (h-stor[n-h] > k) return 0;
+    if (stor[n-h] >= h) return 1;
+    int right = lb(all(stor), h)-stor.begin();
+    int sum = pref[right]-pref[n-h];
+    if ((right-n+h)*h - sum <= k*l) return 1;
+    else return 0;
+}
+void solve() {
+    read(n,k,l);
+    stor.resize(n); read(stor);   
+    sort(all(stor)); 
+    pref.resize(n+1,0);
+
+    FOR(i,1,n+1) pref[i] = pref[i-1] + stor[i-1];
+    int lo = 1, hi = n;
+    while (lo < hi) {
+        int mid = (lo+hi+1)/2;
+        if (good(mid)) lo = mid;
+        else hi = mid-1;
+    }
+    cout << lo << '\n';
+}
+signed main() {
+    #ifdef LOCAL
+    freopen("txt.in","r",stdin);
+    freopen("txt.out","w",stdout);
+    #else
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    #endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
