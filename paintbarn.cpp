14fb88b260b002c7995c330953f4f8d15aeba49f
MC3297 <97566000+MC3297@users.noreply.github.com>
2024-01-27 15:40:47 -0500

    Add files via upload

---
+++ paintbarn.cpp
@@ -0,0 +1,73 @@
+#pragma region
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define int ll
+using pii = pair<int,int>;
+#define vt vector
+#ifndef LOCAL
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#define debug(...)
+#endif
+#define fi first
+#define se second
+#define pb push_back 
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)(x).size()
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define YES cout << "YES" << '\n'
+#define NO cout << "NO" << '\n'
+#define IMP cout << "IMPOSSIBLE" << '\n'
+#define FOR(i,s,e) for (int i = (s); i < (e); ++i) 
+#define F0R(i,e) FOR(i,0,e) 
+#define ROF(i,s,e) for (int i = (e)-1; i >= (s); --i) 
+#define R0F(i,e) ROF(i,0,e) 
+#define printv(v,s,e) FOR(i,s,e+1) cout << v[i] << ' '; cout << '\n'
+#define read(...) int __VA_ARGS__; re(__VA_ARGS__);
+template<class T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+template<class T, class H> istream& operator>>(istream& in, pair<H, T>& a) {in >> a.fi >> a.se; return in;};
+template<typename T> istream& operator>>(istream& in, vector<T>& a) {for(auto &x: a) in >> x; return in;};
+template<class T> void re(T &x) {cin >> x;}
+template<class H, class... T> void re(H &h, T &...t) { re(h); re(t...); }
+#pragma endregion
+
+const int N = 1001;
+int grid[N][N];
+void solve() {
+    read(n,k);
+    F0R(i,n) {
+        read(x1,y1,x2,y2);
+        grid[x1][y1]++;
+        grid[x1][y2]--;
+        grid[x2][y1]--;
+        grid[x2][y2]++;
+    }
+    int ans = 0;
+    F0R(x,N) {
+        F0R(y,N) {
+            if (y > 0) grid[x][y] += grid[x][y-1];
+            if (x > 0) grid[x][y] += grid[x-1][y];
+            if (x > 0 && y > 0) grid[x][y] -= grid[x-1][y-1];
+            if (grid[x][y] == k) ans++;
+        }
+    }
+    cout << ans << '\n';
+}
+signed main() {
+    #ifdef LOCAL
+    freopen("txt.in","r",stdin);
+    freopen("txt.out","w",stdout);
+    #else
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen("paintbarn.in","r",stdin);
+    freopen("paintbarn.out","w",stdout);
+    #endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
